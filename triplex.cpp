494075a81f154045c6d2867006a8404d49edfb40
Shivaadith Anbarasu <shivaadith@gmail.com>
2020-07-09 23:20:32 +0530

    Add initial stage of TripleX project

---
+++ triplex.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() 
+{
+    std::cout << "Hello World";
+    std :: cout << "You are Sam Fisher and you're trying to break into a secure server room!";
+    std :: cout << "Enter the correct codes to continue";
+    return 0;
+}
