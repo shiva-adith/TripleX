4082182a3bc072e61256a99170047545c584781a
Shivaadith Anbarasu <shivaadith@gmail.com>
2020-07-10 00:02:06 +0530

    Update program. Add .gitignore

--- triplex.cpp
+++ triplex.cpp
@@ -2,8 +2,22 @@
 
 int main() 
 {
-    std::cout << "Hello World";
-    std :: cout << "You are Sam Fisher and you're trying to break into a secure server room!";
-    std :: cout << "Enter the correct codes to continue";
+    std::cout << "Welcome Sam fisher";
+    std::cout << std :: endl;
+    std::cout << "You are in the training area where you have to break into a secure server room!";
+    std::cout << std :: endl;
+    std::cout << "Enter the correct codes to continue";
+    std::cout << std::endl;
+
+    int a = 5;
+    int b = 13;
+    int c = 2;
+
+    int sum = a + b + c;
+    int product = a * b * c;
+
+
+    std::cout << sum << std::endl;
+    std::cout << product << std::endl;
     return 0;
 }
