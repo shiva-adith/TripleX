107445f8f8f26f822cfd65696e2335e84d622278
Shivaadith Anbarasu <shivaadith@gmail.com>
2020-07-10 00:58:13 +0530

    Modify Code for game

--- triplex.cpp
+++ triplex.cpp
@@ -1,23 +1,37 @@
+// Preprocessor Directives
 #include <iostream>
 
+// Main Function
 int main() 
 {
-    std::cout << "Welcome Sam fisher";
+    // Expression statements
+    // Print welcome statements to the user
+    std::cout << "Welcome, Sam fisher";
     std::cout << std :: endl;
-    std::cout << "You are in the training area where you have to break into a secure server room!";
+    std::cout << "You are in the training area and your task is to break into a secure server room!" << std::endl;
     std::cout << std :: endl;
-    std::cout << "Enter the correct codes to continue";
+    std::cout << "Enter the correct codes to continue" << std::endl;
     std::cout << std::endl;
 
-    int a = 5;
-    int b = 13;
-    int c = 2;
+    /* Declaration Statements.
 
-    int sum = a + b + c;
-    int product = a * b * c;
+       Declare the three number codes
+       The variable names follow the Unreal Engine
+       naming convention */
 
+    const int CodeA = 5;
+    const int CodeB = 13;
+    const int CodeC = 2;
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
 
-    std::cout << sum << std::endl;
-    std::cout << product << std::endl;
+    // Expression Statements
+    std::cout << "+ There are 3 numbers in the code:" << std::endl;
+    std::cout << "+ The codes add up to: " << CodeSum << std::endl;
+    std::cout << "+ The codes multiply to: " << CodeProduct << std::endl;
+
+    int PlayersGuess;
+
+    // Return Satement
     return 0;
 }
